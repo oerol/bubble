4c8694bbf30870d256b0897ce18a4a23bd668cb5
Özgür Erol <erolxerolx@gmail.com>
2021-12-11 20:02:44 +0100

    basic resizing

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useEffect, useRef } from "react";
 
 function App() {
+  const boxElement = useRef<HTMLDivElement>(null);
+  let positionElement: HTMLDivElement;
+
+  useEffect(() => {});
+
+  const handleBoxMove = (e: React.MouseEvent<HTMLElement>) => {
+    let rect = boxElement.current!.getBoundingClientRect();
+    let y = e.clientY - rect.top;
+    if (y > boxElement.current!.offsetHeight - 10) {
+      boxElement.current!.style.cursor = "ns-resize";
+    } else {
+      boxElement.current!.style.cursor = "default";
+    }
+  };
+
+  const handleBoxDrag = (e: React.MouseEvent<HTMLElement>) => {};
+
+  const handleBoxMouseDown = (e: React.MouseEvent<HTMLElement>) => {
+    document.onmousemove = (e: MouseEvent) => {
+      dragBox(e);
+    };
+  };
+  const dragBox = (e: MouseEvent) => {
+    let rect = boxElement.current!.getBoundingClientRect();
+    let x = e.clientX - rect.left;
+    let y = e.clientY - rect.top;
+
+    /* positionElement = document.getElementById("position") as HTMLDivElement; */
+    /* positionElement.innerText = `x: ${x}, y: ${y}`; */
+
+    boxElement.current!.style.height = `${e.clientY}px`;
+  };
+
+  const handleBoxMouseUp = (e: React.MouseEvent<HTMLElement>) => {
+    document.onmouseup = null;
+    document.onmousemove = null;
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div
+        id="box"
+        onMouseMove={handleBoxMove}
+        onDrag={handleBoxDrag}
+        onMouseDown={handleBoxMouseDown}
+        onMouseUp={handleBoxMouseUp}
+        ref={boxElement}
+      ></div>
+      {/* <div id="position">x: 0, y: 0</div> */}
     </div>
   );
 }
